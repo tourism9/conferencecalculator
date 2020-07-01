6e971029a3d16a3320ea28f1c5394247f1304d22
Andrew Cristopher Keirn <keirna@purdue.edu>
2020-07-01 10:26:19 -0700

    Added User controller, dao, and service

--- backend/src/main/java/tourism9/backend/dao/UserDao.java
+++ backend/src/main/java/tourism9/backend/dao/UserDao.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 public interface UserDao {
     int insertUser(UUID id, User user);
 
-    default int addUser(User user) {
+    default int insertUser(User user) {
         UUID id = UUID.randomUUID();
         return insertUser(id, user);
     }
