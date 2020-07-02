643752045fa01172286d7777c1508faa1fe3c060
Andrew Cristopher Keirn <keirna@purdue.edu>
2020-07-02 13:18:57 -0700

    Added Room model

--- backend/src/main/java/tourism9/backend/model/Room.java
+++ backend/src/main/java/tourism9/backend/model/Room.java
@@ -1,26 +1,59 @@
 package tourism9.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
 public class Room {
 
+    private UUID id;
+    @NotBlank
     private String name;
+    @NotBlank
     private double length;
+    @NotBlank
     private double width;
 
-    public Room(String name, double length, double width) {
+    public Room(@JsonProperty("id") UUID id,
+                @JsonProperty("name") String name,
+                @JsonProperty("length") double length,
+                @JsonProperty("width") double width) {
+        this.id = id;
         this.name = name;
         this.length = length;
         this.width = width;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public double getLength() {
         return length;
     }
 
+    public void setLength(double length) {
+        this.length = length;
+    }
+
     public double getWidth() {
         return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
     }
 }
