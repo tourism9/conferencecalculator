5d3770f304ef4450fc85e9bfdf23aa6443184af4
Zheng <zhengy6@rose-hulman.edu>
2020-07-03 01:38:30 -0700

    added table and add room button

---
+++ backend/src/main/frontend/src/Components/Table.js
@@ -0,0 +1,45 @@
+
+import React, { Component } from 'react';
+import * as ReactBootStrap from "react-bootstrap";
+
+class Table extends Component {
+
+  //update when parameter property changes
+  state={
+    roomsToRender:this.props.roomsToRender
+  }
+
+  render(){
+    const renderRoom=(room, index)=>{
+      console.log("I am doing"+room.name)
+     return ( <tr key={index}> 
+              <td>{room.name}</td>
+              <td>{room.width}</td>
+              <td>{room.length}</td>
+              <td>{room.maxCapacity}</td>
+              </tr> 
+  
+              
+      )
+    }
+    const rooms=this.state.roomsToRender.map(renderRoom)
+    return (
+      <ReactBootStrap.Table striped bordered hover size="sm" responsive style={{ width: "50%", marginLeft: "20%" }} >
+      <thead>
+       <tr>
+          <th>Room Name</th>
+          <th>Room Width</th>
+          <th>Room Length</th>
+          <th>Max Capacity</th>
+        </tr>
+      </thead>
+    <tbody>
+      {rooms}
+    </tbody>
+    </ReactBootStrap.Table>
+
+    )
+  }
+}
+
+export default Table;
