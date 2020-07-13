677148407c693d576aecc5cabc66ac8b6879737f
jurgenkr <jurgenkr@rose-hulman.edu>
2020-07-12 21:20:57 -0400

    Finally got a working database on Heroku There is an odd error that it throws when the BackendApplication is run but it doesn't seem to have an effect on anything

--- src/main/java/tourism9/backend/datasource/PostgresDatasource.java
+++ src/main/java/tourism9/backend/datasource/PostgresDatasource.java
@@ -1,16 +1,25 @@
 package tourism9.backend.datasource;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class PostgresDatasource {
 
     @Bean
-    @ConfigurationProperties("app.datasource")
+    @Primary
+    @ConfigurationProperties("spring.datasource") //it still works with the duplicated prefixes
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource")
     public HikariDataSource hikariDataSource() {
         return DataSourceBuilder
                 .create()
