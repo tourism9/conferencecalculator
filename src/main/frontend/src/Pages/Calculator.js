e16d34fdf7bfa36504f801f24be17b4f289e53c3
Zheng <zhengy6@rose-hulman.edu>
2020-07-13 20:42:44 -0700

    added units to room for frontend

--- src/main/frontend/src/Pages/Calculator.js
+++ src/main/frontend/src/Pages/Calculator.js
@@ -23,7 +23,7 @@ function Calculator() {
 
     const addNewRoom=()=>{
       
-      //rooms.push(newRoomData);
+    
      //use 'http://localhost:8080/api/v1/room' when testing on local web browser. 
      fetch( 'https://conferencecalculator.herokuapp.com/api/v1/room',{
        method:'POST',
