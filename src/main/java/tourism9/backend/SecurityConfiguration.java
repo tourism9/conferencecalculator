e63cdfda96ed9fd34fc3266845b4fdf9488bc6e0
Andrew Cristopher Keirn <keirna@purdue.edu>
2020-07-18 10:55:42 -0700

    Added SecurityConfiguration

---
+++ src/main/java/tourism9/backend/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package tourism9.backend;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication();
+    }
+}
