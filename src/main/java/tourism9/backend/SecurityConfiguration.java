883342b790e7bddc137becd6c573be51c11e360a
Andrew Cristopher Keirn <keirna@purdue.edu>
2020-07-18 11:20:57 -0700

    Implemented password encoding

--- src/main/java/tourism9/backend/SecurityConfiguration.java
+++ src/main/java/tourism9/backend/SecurityConfiguration.java
@@ -1,9 +1,38 @@
 package tourism9.backend;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tourism9.backend.service.UserService;
 
+import javax.annotation.Resource;
+
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Resource
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(20);
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication();
--- src/main/java/tourism9/backend/service/UserService.java
+++ src/main/java/tourism9/backend/service/UserService.java
@@ -2,18 +2,22 @@ package tourism9.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import tourism9.backend.dao.UserDao;
 import tourism9.backend.model.User;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Service
+@Service("userService")
 public class UserService {
 
+    @Qualifier("realUserDao")
+    @Autowired
     private final UserDao userDao;
 
     @Autowired
@@ -21,8 +25,11 @@ public class UserService {
         this.userDao = userDao;
     }
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     public int addUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userDao.insertUser(user);
     }
 
